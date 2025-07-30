28820d0f18177e3f1100d24c135644f5e192990e
alya <aliya2002mah@gmail.com>
2025-07-30 14:50:50 +0300

    '

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from 'react';
+import * as weatherService from './services/weatherService';
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+
 const App = () => {
+  const [weather, setWeather] = useState('');
 
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+useEffect(() => {
 
-export default App
+    const fetchDefaultData = async () => {
+      const data = await weatherService.show('New York');
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    };
+
+    fetchDefaultData();
+
+  }, []);
+
+
+return (
+  <main>
+    <h1>Weather API</h1>
+    <WeatherSearch />
+    <WeatherDetails weather={weather} />
+  </main>
+);
+
+};
+
+export default App;
